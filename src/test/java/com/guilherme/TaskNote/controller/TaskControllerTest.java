75dce4f55f16f1f7a374b4ac98cd4cad2a0dbd5f
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-11 15:01:58 -0300

    Adicionando o Mockito

--- src/test/java/com/guilherme/TaskNote/controller/TaskControllerTest.java
+++ src/test/java/com/guilherme/TaskNote/controller/TaskControllerTest.java
@@ -1,7 +1,32 @@
 package com.guilherme.TaskNote.controller;
 
+import com.guilherme.TaskNote.TaskNoteApplication;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class TaskControllerTest {
 
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = TaskNoteApplication.class)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class TaskControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+
+    }
 }

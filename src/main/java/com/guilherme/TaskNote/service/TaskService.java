8400c25e839088bd5ac94f20deb66de2f4b48e5e
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-11 14:33:47 -0300

    metodos do crud da interface task

--- src/main/java/com/guilherme/TaskNote/service/TaskService.java
+++ src/main/java/com/guilherme/TaskNote/service/TaskService.java
@@ -1,4 +1,12 @@
 package com.guilherme.TaskNote.service;
 
+import com.guilherme.TaskNote.Model.Task;
+
+import java.util.List;
+
 public interface TaskService {
+    public List<Task> getAllToDo();
+    public Task getToDoById(Long id);
+    public Task saveToDo(Task task);
+    public void removeToDo(Task task);
 }

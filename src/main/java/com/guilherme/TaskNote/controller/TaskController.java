92f7baeaa6b375b670f684c986515f32882bf0ea
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-11 14:55:41 -0300

    criando o metodo de listar todos as tasks do controller

--- src/main/java/com/guilherme/TaskNote/controller/TaskController.java
+++ src/main/java/com/guilherme/TaskNote/controller/TaskController.java
@@ -1,12 +1,28 @@
 package com.guilherme.TaskNote.controller;
 
+import com.guilherme.TaskNote.Model.Task;
+import com.guilherme.TaskNote.service.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
+    @Autowired
+    private TaskService taskService;
 
+    @RequestMapping(value="/task", method= RequestMethod.GET)
+    public ResponseEntity<List<Task>> getAllTasks(){
+        logger.info("Monstrando todas as Tasks");
+        return new ResponseEntity<List<Task>>(taskService.getAllTask(), HttpStatus.OK);
+    }
 }

87066ad1d7a05f978b73af6da48d947ec2a6467a
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-11 16:11:16 -0300

    criando metodo de buscar pelo id

--- src/main/java/com/guilherme/TaskNote/controller/TaskController.java
+++ src/main/java/com/guilherme/TaskNote/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.guilherme.TaskNote.controller;
 
 import com.guilherme.TaskNote.Model.Task;
+import com.guilherme.TaskNote.exception.TaskException;
 import com.guilherme.TaskNote.service.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,13 @@ public class TaskController {
         return new ResponseEntity<List<Task>>(taskService.getAllTask(), HttpStatus.OK);
     }
 
-
+    @RequestMapping(value = "/task/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Task> getToDoById(@PathVariable("id") Long id) throws TaskException {
+        logger.info("Task id " + id);
+        Task task = (Task) taskService.getTaskById(id);
+        if (task == null || task.getId() <= 0){
+            throw new TaskException("Task não encontrada");
+        }
+        return new ResponseEntity<Task>((Task) taskService.getTaskById(id), HttpStatus.OK);
+    }
 }

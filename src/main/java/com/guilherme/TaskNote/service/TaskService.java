fa9dea892a80a42d77bf3b2d15601932366f0eb3
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-11 14:35:22 -0300

    Edit names

--- src/main/java/com/guilherme/TaskNote/service/TaskService.java
+++ src/main/java/com/guilherme/TaskNote/service/TaskService.java
@@ -1,12 +1,14 @@
 package com.guilherme.TaskNote.service;
 
 import com.guilherme.TaskNote.Model.Task;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface TaskService {
-    public List<Task> getAllToDo();
-    public Task getToDoById(Long id);
-    public Task saveToDo(Task task);
-    public void removeToDo(Task task);
+    public List<Task> getAllTask();
+    public Task getTaskById(Long id);
+    public Task saveTask(Task task);
+    public void removeTask(Task task);
 }

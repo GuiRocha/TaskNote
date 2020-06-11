8d4f80a95c5fd79bd2ed0258491599881f3f6480
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-11 15:07:13 -0300

    Testando a verificação de listar todas as tasks

--- src/test/java/com/guilherme/TaskNote/controller/TaskControllerTest.java
+++ src/test/java/com/guilherme/TaskNote/controller/TaskControllerTest.java
@@ -2,16 +2,23 @@ package com.guilherme.TaskNote.controller;
 
 import com.guilherme.TaskNote.TaskNoteApplication;
 import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
 //@RunWith(SpringRunner.class)
@@ -28,5 +35,11 @@ class TaskControllerTest {
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 
+    }
+
+    @Test
+    public void verifyAllTasks() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/task").accept(MediaType.APPLICATION_JSON))
+                .andExpect((ResultMatcher) MockRestRequestMatchers.jsonPath("$", hasSize(4))).andDo(print());
     }
 }
